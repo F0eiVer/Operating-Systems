183a153ea57b68349de3948df6c0b3594ac0b4a2
F0eiVer <arefev030303@mail.ru>
2025-01-08 17:42:02 +1000

    init third lab

---
+++ lab3/programm.cpp
@@ -0,0 +1,4 @@
+#include "programm.hpp"
+
+using namespace TimerProgramm;
+
---
+++ lab3/programm.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#endif
+
