4d2ae4614ab517cc5455228968f1bbc2dd09b430
F0eiVer <arefev030303@mail.ru>
2025-01-10 23:49:49 +1000

    add get_time function on windows

---
+++ lab3/build/CMakeFiles/TimerProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TimerProject.dir/main.cpp.obj"
+  "CMakeFiles/TimerProject.dir/main.cpp.obj.d"
+  "CMakeFiles/TimerProject.dir/programm.cpp.obj"
+  "CMakeFiles/TimerProject.dir/programm.cpp.obj.d"
+  "TimerProject.exe"
+  "TimerProject.exe.manifest"
+  "TimerProject.pdb"
+  "libTimerProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TimerProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab3/build/CMakeFiles/TimerProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TimerProject.
+# This may be replaced when dependencies are built.
