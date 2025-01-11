9a5579cb744ca60e31b536f9ddc585713ce869df
F0eiVer <arefev030303@mail.ru>
2025-01-11 17:49:25 +1000

    add threads lib from examples

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include "programm.hpp"
 #include "my_shmem.hpp"
+#include "my_thread.hpp"
+
+using std::cin;
 
 struct Counter{
   int count = 0;
@@ -17,6 +20,19 @@ void write(std::string str) {
 
 int main(int argc, char** argv) {
   auto shmem = cplib::SharedMem<Counter>("shmem");
+  std::string cmd = "";
+  int iparam = 0;
+
+  while(true){
+    std::cin >> cmd;
+		if (cmd == "i" || cmd == "int") {
+			std::cin >> iparam;
+		} else if (cmd == "e" || cmd == "q" || cmd == "exit" || cmd == "quite") {
+			break;
+		} else{
+			std::cout << "! Possible commands: i | q" << std::endl;
+    }
+  }
 
   write(TimerProgramm::get_cur_time());
   return 0;
